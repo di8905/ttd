9f8a17503dbeb69935f441655e74191b39110562
Di <pdv.spb@gmail.com>
2016-08-30 14:31:07 +0300

    Task done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  root 'welcome#index'
+  resources :trains
   resources :railway_stations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
