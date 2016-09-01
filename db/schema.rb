4c0d3bde9eab1b2a4034a8a93a26e325dd08616a
Di <pdv.spb@gmail.com>
2016-09-01 15:46:18 +0300

    Ticket addet into DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901122833) do
+ActiveRecord::Schema.define(version: 20160901124240) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20160901122833) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "description"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trains", force: :cascade do |t|
