4e9fc36a631eb53a3bbbf2e2a3a9826bdd7d9207
Di <pdv.spb@gmail.com>
2016-09-06 15:16:39 +0300

    Train anonymous partial done

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,4 +4,6 @@ class RailwayStation < ActiveRecord::Base
   has_many :routes, through: :railway_stations_routes
   has_many :tickets_to_here, class_name: 'Ticket', foreign_key: :destination_station_id
   has_many :tickets_from_here, class_name: 'Ticket', foreign_key: :departure_station_id
+
+  validates :title, presence: true
 end
