dd67dd26361385ce146ef02baf8b400702075c30
Di <pdv.spb@gmail.com>
2016-09-06 14:05:21 +0300

    Train model number validation added

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,4 +2,6 @@ class Train < ActiveRecord::Base
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
   belongs_to :route
   has_many :tickets
+
+  validates :number, presence: true
 end
