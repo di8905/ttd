b8d0a300d027bfde4fc32bd8fa79b2ab404e4967
Di <pdv.spb@gmail.com>
2016-09-07 11:29:10 +0300

    Routes description added

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -43,6 +43,6 @@ class RoutesController < ApplicationController
   end
 
   def route_params
-    params.require(:route).permit(:name)
+    params.require(:route).permit(:name, :description)
   end
 end
