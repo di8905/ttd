d485e8e876fe19e68e17f5ffd0616483463de91f
Di <pdv.spb@gmail.com>
2016-09-12 21:47:49 +0300

    Wagons route added

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :trains
   resources :railway_stations
   resources :routes
+  resources :wagons
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
