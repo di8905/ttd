6bb08749d3da5333f6adca99094ed12dcb2b6332
Di <pdv.spb@gmail.com>
2016-09-13 22:21:55 +0300

    Wagon show controller and view done

--- app/controllers/wagons_controller.rb
+++ app/controllers/wagons_controller.rb
@@ -1,4 +1,5 @@
 class WagonsController < ApplicationController
+before_action :set_wagon, only: [:show]
 
   def index
     @wagons = Wagon.all
@@ -6,6 +7,26 @@ class WagonsController < ApplicationController
 
   def new
     @wagon = Wagon.new
+  end
+
+  def create
+    @wagon = Wagon.new(wagon_params)
+    redirect_to wagons_url
+  end
+
+  def show
+  end
+
+
+
+private
+
+  def set_wagon
+    @wagon = Wagon.find(params[:id])
+  end
+
+  def wagon_params
+    params.require(:wagon).permit(:w_type, :upper_seats, :lower_seats)
   end
 
 end
