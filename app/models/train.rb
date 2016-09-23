198e1eb6e97b57880f528d05f4786f00d9b0951c
Di <pdv.spb@gmail.com>
2016-09-23 13:28:33 +0300

    Lost seats counter method cherry-picked

--- app/models/train.rb
+++ app/models/train.rb
@@ -5,17 +5,7 @@ class Train < ActiveRecord::Base
   has_many :carriages
 
   validates :number, presence: true
-
-
-
-  # def carriage_count(carriage_type)
-  #   count = 0
-  #   self.carriages.each do |carriage|
-  #     count += 1 if carriage.w_type == carriage_type
-  #   end
-  #   count
-  # end
-  #
+  
   def seats_count(carriage_type, seat_type)
     self.carriages.where(type: carriage_type).pluck(seat_type.to_sym).inject(:+)
   end
