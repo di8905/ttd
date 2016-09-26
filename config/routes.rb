ff9f0ec4549960f689cf1f938d2b89b16b755e73
Di <pdv.spb@gmail.com>
2016-09-26 10:28:40 +0300

    Routes show remastered to slim

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  post 'update_position/:id', to: 'railway_stations#update_position', as: 'update_position'
+  # post 'update_position/:id', to: 'railway_stations#update_position', as: 'update_position'
   root 'welcome#index'
   resources :trains
-  resources :railway_stations
+  resources :railway_stations do
+    patch :update_position, on: :member
+  end
   resources :routes
   resources :carriages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
